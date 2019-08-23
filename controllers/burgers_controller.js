4274727d4099118e1726408cc79b9a0b1a626b05
Nich Secord <nicholassecord@gmail.com>
2019-08-22 18:36:15 -0700

    app finished... i think

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,23 +1,20 @@
 const express = require('express');
+const path = require('path');
 const { selectAllBurgers, insertOneBurger, updateOneBurgerDevoured } = require('../models/burger');
 
 const router = express.Router();
 
-router.get('/', (req, res) => selectAllBurgers((dbRes) => res.json(dbRes)));
+//* Set Static
+const static = express.static(path.join(path.dirname(require.main.filename), '/public'));
 
-router.post('/', (req, res) => {
-    const { name } = req.body;
+//* Page Route
+router.get('/', (req, res) => selectAllBurgers((dbRes) => res.render('burgerList', { burgers: dbRes })));
 
-    insertOneBurger(name);
+//* API Routes
+router.get('/api/burgers', (req, res) => selectAllBurgers((dbRes) => res.json(dbRes)));
 
-    res.send(`${name} added`);
-});
+router.post('/api/burgers', (req, res) => insertOneBurger(req.body.name));
 
-router.put('/', (req, res) => {
-    const { id } = req.body;
-    updateOneBurgerDevoured(id);
+router.put('/api/burgers', (req, res) => updateOneBurgerDevoured(req.body.id));
 
-    res.send(`Burger with the id of ${id} has been marked as devoured!`);
-});
-
-module.exports = router;
+module.exports = { router, static };
