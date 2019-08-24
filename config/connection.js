ba8286a222fcf6f0affca4fa225dec7f167dded4
Nich Secord <nicholassecord@gmail.com>
2019-08-24 07:25:04 -0700

    adding jaws_db ref

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'nich',
-    password: '123456',
-    database: 'burgers_db',
-});
+const db = process.env.JAWSDB_URL
+    ? mysql.createConnection(process.env.JAWSDB_URL)
+    : mysql.createConnection({
+          host: 'localhost',
+          user: 'nich',
+          password: '123456',
+          database: 'burgers_db',
+      });
 
 module.exports = db;
